e1a35916e92168742fd2450545f09e843dce93be
ElJuanchito <perdomocardenas18@gmail.com>
2024-03-16 18:54:11 -0500

    load backend protype

---
+++ backend/crud-productos/src/test/java/co/edu/uniquindio/crudproductos/CrudProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.crudproductos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
